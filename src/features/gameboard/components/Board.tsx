da19127a319da1d05e3e7a26e2a3a846745eb3e9
Chris <cmerrill99@gmail.com>
2023-10-14 14:03:31 -0500

    Arrays being objects is wild sometimes

--- src/features/gameboard/components/Board.tsx
+++ src/features/gameboard/components/Board.tsx
@@ -29,19 +29,19 @@ const Board = () => {
   return (
     <>
       <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => onSquareClick(0)} />
         <Square value={squares[1]} onSquareClick={() => onSquareClick(1)} />
         <Square value={squares[2]} onSquareClick={() => onSquareClick(2)} />
-        <Square value={squares[3]} onSquareClick={() => onSquareClick(3)} />
       </div>
       <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => onSquareClick(3)} />
         <Square value={squares[4]} onSquareClick={() => onSquareClick(4)} />
         <Square value={squares[5]} onSquareClick={() => onSquareClick(5)} />
-        <Square value={squares[6]} onSquareClick={() => onSquareClick(6)} />
       </div>
       <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => onSquareClick(6)} />
         <Square value={squares[7]} onSquareClick={() => onSquareClick(7)} />
         <Square value={squares[8]} onSquareClick={() => onSquareClick(8)} />
-        <Square value={squares[9]} onSquareClick={() => onSquareClick(9)} />
       </div>
     </>
   );
