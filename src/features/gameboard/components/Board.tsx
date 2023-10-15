d1173a733486dfc507ed17f6eff3fe4dac8e2f11
Chris <cmerrill99@gmail.com>
2023-10-14 20:35:42 -0500

    Finally figured out looping!

--- src/features/gameboard/components/Board.tsx
+++ src/features/gameboard/components/Board.tsx
@@ -35,24 +35,29 @@ const Board = ({
     }
   };
 
+  const boardRows: (Player | undefined)[][] = [0, 1, 2].map((rowIndex) => {
+    const rowStart = rowIndex * 3;
+    return squares.slice(rowStart, rowStart + 3);
+  });
+
   return (
     <>
       <div className="status">{getStatus()}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => onSquareClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => onSquareClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => onSquareClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => onSquareClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => onSquareClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => onSquareClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => onSquareClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => onSquareClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => onSquareClick(8)} />
-      </div>
+      {boardRows.map((row, rowIndex) => {
+        return (
+          <div key={rowIndex} className="board-row">
+            {row.map((square, colIndex) => {
+              return (
+                <Square
+                  key={rowIndex * 3 + colIndex}
+                  value={square}
+                  onSquareClick={() => onSquareClick(rowIndex * 3 + colIndex)}
+                />
+              );
+            })}
+          </div>
+        );
+      })}
     </>
   );
 };
