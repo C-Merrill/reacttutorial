17bb4ecb53d23ef3090534c4ee14876f81c6ef29
Chris <cmerrill99@gmail.com>
2023-10-14 19:17:36 -0500

    Move win calculation to helper

--- src/features/gameboard/components/Board.tsx
+++ src/features/gameboard/components/Board.tsx
@@ -1,46 +1,7 @@
 import {useState} from 'react';
 import Square from './Square';
 import {Turn, getTurnDisplay} from '../model/turn';
-
-function getRowForIndex(index: number): [number, number, number] {
-  const rowStart = Math.floor(index / 3) * 3;
-  return [rowStart, rowStart + 1, rowStart + 2];
-}
-
-function getColForIndex(index: number): [number, number, number] {
-  const colStart = index % 3;
-  return [colStart, colStart + 3, colStart + 6];
-}
-
-function getDiagsForIndex(index: number): [number, number, number][] {
-  const possibleDiags: [number, number, number][] = [
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  return possibleDiags.filter((diag) => diag.includes(index));
-}
-
-const calculateWinner = (
-  squares: (Turn | undefined)[],
-  indexOfMove: number,
-): Turn | undefined => {
-  const linesToCheck = [
-    ...getDiagsForIndex(indexOfMove),
-    getColForIndex(indexOfMove),
-    getRowForIndex(indexOfMove),
-  ];
-
-  for (let i = 0; i < linesToCheck.length; i++) {
-    const [a, b, c] = linesToCheck[i];
-    if (
-      squares[a] !== undefined &&
-      squares[a] === squares[b] &&
-      squares[a] === squares[c]
-    ) {
-      return squares[a];
-    }
-  }
-};
+import calculateWinner from '../model/calculateWinner';
 
 const Board = () => {
   const [turn, setTurn] = useState<Turn>(Turn.X);
